cc3ebf3451393618de449a775a4b05a1f7c50feb
Simona Pizio <simona.pizio@gmail.com>
2022-03-09 17:45:56 +0100

    Pass the search results and show results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,22 +1,30 @@
 //import logo from './logo.svg';
+import React from 'react';
 import { Playlist } from '../Playlist/Playlist.js';
 import { SearchBar } from '../SearchBar/SearchBar.js';
 import { SearchResults } from '../SearchResults/SearchResults.js';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist /> 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchResults: [{name: ''}, {artist: ''}, {album: ''}, {id: ''}]};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist /> 
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Track } from "../Track/Track";
 import './TrackList.css';
 
 export class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-          -- You will add a map method that renders a set of Track components  --
+        {this.props.tracks.map(function(track){ 
+          return  <Track track={this.props.track} key={this.props.track.id}/>;
+        })}
       </div>
     );
   }
