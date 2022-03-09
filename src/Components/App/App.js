5a9e89e19da4c13deed3ab7438e78568bb61bbe1
Simona Pizio <simona.pizio@gmail.com>
2022-03-09 15:24:16 +0100

    modified App Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        Add a SearchBar component 
+        <div className="App-playlist">
+          Add a SearchResults component 
+          Add a Playlist component 
+        </div>
+      </div>
     </div>
   );
 }
