241304dc02b50b5c189aa4326c6608d8e9c6c91e
Simona Pizio <simona.pizio@gmail.com>
2022-03-10 09:29:21 +0100

    Change App to function in component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,22 +1,25 @@
 //import logo from './logo.svg';
+import React from 'react';
 import { Playlist } from '../Playlist/Playlist.js';
 import { SearchBar } from '../SearchBar/SearchBar.js';
 import { SearchResults } from '../SearchResults/SearchResults.js';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist /> 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist /> 
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
