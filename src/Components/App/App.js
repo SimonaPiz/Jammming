1069b5751a2bfcedb1474dcdd15023d5a6ec9716
Simona Pizio <simona.pizio@gmail.com>
2022-03-11 09:06:50 +0100

    Add method savePlaylist for save playlist to spotify

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -52,6 +53,11 @@ class App extends React.Component {
     this.setState({playlistName: name});
   }
 
+  savePlaylist() {
+    const plTracks = this.state.playlistTracks;
+    const trackURIs = plTracks.map(plTrack => plTrack.uri);
+  }
+
   render() {
     return (
       <div>
@@ -64,7 +70,8 @@ class App extends React.Component {
             <Playlist playlistTracks={this.state.playlistTracks} 
                       playlistName={this.state.playlistName}
                       onRemove={this.removeTrack}
-                      onNameChange={this.updatePlaylistName} /> 
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist} /> 
           </div>
         </div>
       </div>
