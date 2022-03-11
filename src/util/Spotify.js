8d8306b1b8dc5611d2b759c1569b4b53839116c2
Simona Pizio <simona.pizio@gmail.com>
2022-03-11 15:56:03 +0100

    Create util-Spotify and request token to access it

---
+++ src/util/Spotify.js
@@ -0,0 +1,29 @@
+let accessToken;
+const clientId = '886fa341ef8d4583ac685605960aa9a4';
+const redURI = 'http://localhost:3000/';
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    };
+
+    // check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redURI}`;
+      window.location = accessUrl;
+    }
+  }
+};
+
+export default Spotify;
